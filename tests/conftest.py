1c862e57116fdb09d303d5356a903bb94fe99d0e
SEBROVATER <sebrovskiy.k@gmail.com>
2024-02-26 23:55:27 +0600

    feat: added connection to test database for test client

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,22 @@
 import pytest
 from fastapi.testclient import TestClient
+from tortoise.contrib.fastapi import register_tortoise
 
-from app.main import app
+from app.main import get_app
 
 
 @pytest.fixture(scope="session")
 def client():
+    config = {
+        "connections": {"default": "sqlite://:memory:"},
+        "apps": {
+            "models": {
+                "models": ["app.expenses.models"],
+                "default_connection": "default",
+            },
+        },
+    }
+    app = get_app()
+    register_tortoise(app, config)
     with TestClient(app) as client:
         yield client
