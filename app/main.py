4c188ab57b7c98eb64809f41e6d2466150c5782e
SEBROVATER <sebrovskiy.k@gmail.com>
2024-02-26 23:56:46 +0600

    feat: added connection to database for app

--- app/main.py
+++ app/main.py
@@ -1,24 +1,30 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from tortoise.contrib.fastapi import register_tortoise
 
 from app.config import Settings
-from app.routes import router
-from fastapi.staticfiles import StaticFiles
+from app.db import TORTOISE_ORM
+from app.expenses.routes import router as expenses_router
+from app.routes import router as main_router
+
 settings = Settings()
+
 
 def get_app() -> FastAPI:
     """Create a FastAPI app with the specified settings."""
 
     app = FastAPI(**settings.fastapi_kwargs)
     app.mount("/static", StaticFiles(directory=settings.APP_DIR / "static"), name="static")
-    app.include_router(router)
+    app.include_router(main_router)
+    app.include_router(expenses_router)
 
     return app
 
 
 app = get_app()
 
-
 if __name__ == "__main__":
     import uvicorn
 
+    register_tortoise(app, config=TORTOISE_ORM)
     uvicorn.run(app, host="127.0.0.1", port=8000)
