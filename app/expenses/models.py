36b1da0563c0a8fb742d18feca94803d0f67f9bc
SEBROVATER <sebrovskiy.k@gmail.com>
2024-03-04 13:00:05 +0600

    feat: removed 'source' model

--- app/expenses/models.py
+++ app/expenses/models.py
@@ -3,11 +3,8 @@ from enum import Enum
 
 from tortoise.fields import (
     CharEnumField,
-    CharField,
     DatetimeField,
     DecimalField,
-    ForeignKeyField,
-    OnDelete,
     TextField,
 )
 from tortoise.models import Model
@@ -26,28 +23,12 @@ class TimestampMixin:
     updated_at = DatetimeField(auto_now=True, null=False)
 
 
-class AmountMixin:
+class Expense(TimestampMixin, Model):
+    name = TextField(null=False)
     amount: Decimal = DecimalField(max_digits=20, decimal_places=10, null=False)
     currency: Currency = CharEnumField(Currency, max_length=5, null=False)
-
-
-class Source(TimestampMixin, AmountMixin, Model):
-    name = CharField(max_length=32)
-
-    def __str__(self) -> str:
-        name = self.name
-        if len(name) > 7:
-            name = f"{name[:7]}..."
-        return f"{name} ({self.amount}{self.currency})"
-
-
-class Expense(TimestampMixin, AmountMixin, Model):
-    name = TextField(null=False)
     count: Decimal = DecimalField(
         max_digits=20, decimal_places=10, null=False, default=Decimal(1.0)
-    )
-    source = ForeignKeyField(
-        "models.Source", related_name="expenses", null=True, on_delete=OnDelete.SET_NULL
     )
 
     date = DatetimeField(auto_now_add=True)
