ba1d706194b0970366b27b908004634fd7575075
SEBROVATER <sebrovskiy.k@gmail.com>
2024-04-20 15:26:20 +0600

    fix: populate with zero for remained currencies

--- app/dashboards/routes.py
+++ app/dashboards/routes.py
@@ -3,7 +3,7 @@ from datetime import UTC, datetime, timedelta
 from fastapi import APIRouter, Request
 from tortoise.functions import Sum
 
-from app.expenses.models import Expense
+from app.expenses.models import Currency, Expense
 from app.routes import templates
 
 router = APIRouter()
@@ -22,6 +22,10 @@ async def get_dashboards(request: Request):
     ):
         totals_month[data["currency"]] = data["total"]
 
+    for currency in Currency:
+        if currency not in totals_month:
+            totals_month[currency] = 0.0
+
     totals_2months = dict()
     for data in (
         await Expense.filter(date__gt=now - timedelta(days=62))
@@ -30,6 +34,11 @@ async def get_dashboards(request: Request):
         .values("currency", "total")
     ):
         totals_2months[data["currency"]] = data["total"]
+
+    for currency in Currency:
+        if currency not in totals_2months:
+            totals_2months[currency] = 0.0
+
     totals_3months = dict()
     for data in (
         await Expense.filter(date__gt=now - timedelta(days=93))
@@ -38,6 +47,10 @@ async def get_dashboards(request: Request):
         .values("currency", "total")
     ):
         totals_3months[data["currency"]] = data["total"]
+
+    for currency in Currency:
+        if currency not in totals_3months:
+            totals_3months[currency] = 0.0
 
     totals_half_year = dict()
     for data in (
@@ -48,6 +61,10 @@ async def get_dashboards(request: Request):
     ):
         totals_half_year[data["currency"]] = data["total"]
 
+    for currency in Currency:
+        if currency not in totals_half_year:
+            totals_half_year[currency] = 0.0
+
     totals_year = dict()
     for data in (
         await Expense.filter(date__gt=now - timedelta(days=365))
@@ -56,18 +73,25 @@ async def get_dashboards(request: Request):
         .values("currency", "total")
     ):
         totals_year[data["currency"]] = data["total"]
+
+    for currency in Currency:
+        if currency not in totals_year:
+            totals_year[currency] = 0.0
+
     totals_overall = dict()
     for data in (
         await Expense.annotate(total=Sum("amount")).group_by("currency").values("currency", "total")
     ):
         totals_overall[data["currency"]] = data["total"]
 
-    relevant_currencies = await Expense.all().distinct().values_list("currency", flat=True)
+    for currency in Currency:
+        if currency not in totals_overall:
+            totals_overall[currency] = 0.0
+
     return templates.TemplateResponse(
         request,
         "dashboards/main.html",
         context={
-            "relevant_currencies": relevant_currencies,
             "totals_month": totals_month,
             "totals_2months": totals_2months,
             "totals_3months": totals_3months,
